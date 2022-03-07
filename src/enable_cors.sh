7e5850ed97f89aa658771dbe8c36ac08d84b4d53
Luan Eduardo da Costa <luan1346@gmail.com>
2022-03-07 10:54:47 -0300

    Improve enable_cors.sh script

--- src/enable_cors.sh
+++ src/enable_cors.sh
@@ -1,1 +1,13 @@
-kubectl get deployments -A | awk '{if (NR!=1) {print " -n " $1 " set env deployment/" $2 " ENABLE_CORS=1"}}' | xargs -L 1 kubectl
+#!/bin/bash
+
+read -p "Should enable or disable CORS (1 to enable, 0 to disable): " should_enable
+
+if [ "$should_enable" = "" ]; then
+    echo 'missing $should_enable'
+    exit 0
+elif [ "$should_enable" != "0" -a "$should_enable" != "1" ]; then
+    echo '$should_enable must be 0 or 1'
+    exit 0
+fi
+
+kubectl get deployments -A | awk '{if (NR!=1) {print " -n " $1 " set env deployment/" $2 " ENABLE_CORS=" $should_enable}}' | xargs -L 1 kubectl
